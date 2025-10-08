ab5f9cc897b2ab20a8fb5a9643c1c0b07c6e6720
Edgar Ampiire <eampiire@gmail.com.com>
2025-10-08 15:14:15 +0300

    updated routes & tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,58 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_task, only: [ :show, :edit, :update, :destroy, :toggle_complete ]
+
+  def index
+    @tasks = current_user.tasks.order(created_at: :desc)
+  end
+
+  def show
+    @comment = Comment.new
+  end
+
+  def new
+    @task = current_user.tasks.build
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to @task, notice: "Task created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: "Task Updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: "Task Deleted."
+  end
+
+  def toggle_complete
+    @task.update(completed: !@task.completed)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to tasks_path }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
 end
