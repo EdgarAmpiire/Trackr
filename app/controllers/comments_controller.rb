9e84be4ac064cf5188cad45e30d6747a7767090f
Edgar Ampiire <eampiire@gmail.com.com>
2025-10-08 15:44:33 +0300

    updated comments  controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,37 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_task
+
+  def create
+    @comment = @task.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @task }
+      end
+    else
+      redirect_to @task, alert: "Comment cannot be empty."
+    end
+  end
+
+  def destroy
+    @comment = task.comments.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to @task }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
